2096a5bdaa476fffe32f22be7b3d004391d946f7
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-07-12 10:30:34 +0000

    [add]Ajax in DM

--- app/controllers/public/messages_controller.rb
+++ app/controllers/public/messages_controller.rb
@@ -1,11 +1,18 @@
 class Public::MessagesController < ApplicationController
 
   def create
+    @messages = Message.where(room_id: params[:message][:room_id])
     if Entry.where(member_id: current_member.id, room_id: params[:message][:room_id]).exists?
-      @message = Message.create(params.require(:message).permit(:user_id, :message, :room_id).merge(member_id: current_member.id))
+      message = Message.create((message_params).merge(member_id: current_member.id))
+      message.save
     else
       flash[:notice] = "メッセージ送信に失敗しました。"
     end
-    redirect_to room_path(@message.room_id)
+  end
+
+  private
+
+  def message_params
+    params.require(:message).permit(:user_id, :message, :room_id)
   end
 end
--- app/controllers/public/rooms_controller.rb
+++ app/controllers/public/rooms_controller.rb
@@ -3,18 +3,24 @@ class Public::RoomsController < ApplicationController
   def create
     @room =                 Room.create
     @current_member_entry = Entry.create(room_id: @room.id, member_id: current_member.id)
-    @member_entry =         Entry.create(params.require(:entry).permit(:member_id, :room_id).merge(room_id: @room.id))
+    @member_entry =         Entry.create((room_params).merge(room_id: @room.id))
     redirect_to room_path(@room.id)
   end
 
   def show
     @room = Room.find(params[:id])
     if Entry.where(member_id: current_member.id, room_id: @room.id).exists?
-      @messages = @room.messages
-      @message  = Message.new
-      @entries  = @room.entries
+      @messages =     @room.messages
+      @new_message  = Message.new
+      @entries  =     @room.entries
     else
       redirect_back(fallback_location: root_path)
     end
+  end
+
+  private
+
+  def room_params
+    params.require(:entry).permit(:member_id, :room_id)
   end
 end

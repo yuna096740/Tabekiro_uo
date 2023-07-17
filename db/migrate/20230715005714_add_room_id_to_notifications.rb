88d15650942143df3a33621d3d342840c2040f0c
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-07-17 10:25:00 +0000

    [fix]add_room_id_to_notifications

--- db/migrate/20230715005714_add_room_id_to_notifications.rb
+++ db/migrate/20230715005714_add_room_id_to_notifications.rb
@@ -2,8 +2,8 @@ class AddRoomIdToNotifications < ActiveRecord::Migration[6.1]
   def change
     add_column :notifications, :room_id, :integer
     add_column :notifications, :message_id, :integer
-  end
 
-  add_index :notifications, :room_id
-  add_index :notifications, :message_id
+    add_index :notifications, :room_id
+    add_index :notifications, :message_id
+  end
 end

a91a1ab89236a5660acf6c6dda0b99f83a4c44b2
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-07-24 09:08:44 +0000

    [add] validation and before_action :valid_id? in members_controller

--- app/models/member.rb
+++ app/models/member.rb
@@ -4,6 +4,8 @@ class Member < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  validates :email,           presence: :true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :nickname,        presence: :true, length: { in: 1..15 }
   validates :introduction,    length: { maximum: 80 }
 

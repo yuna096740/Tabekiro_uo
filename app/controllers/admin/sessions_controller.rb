54f611d65e57db1eda98f01458f131713a5dd390
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-07-24 05:01:30 +0000

    [fix] after_sign_in_path_for

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -2,9 +2,9 @@
 
 class Admin::SessionsController < Devise::SessionsController
   # before_action :configure_sign_in_params, only: [:create]
-  # def after_sign_in_path_for(resource)
-  #   admin_path
-  # end
+  def after_sign_in_path_for(resource)
+    admin_path
+  end
 
   # GET /resource/sign_in
   # def new

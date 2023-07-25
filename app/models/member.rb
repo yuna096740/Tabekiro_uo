e70fba483ac602576216386afa4f15370372b6b6
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-07-25 00:19:12 +0000

    [save]

--- app/models/member.rb
+++ app/models/member.rb
@@ -7,7 +7,6 @@ class Member < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   validates :email,           presence: :true, format: { with: VALID_EMAIL_REGEX }, uniqueness: { case_sensitive: false }
   validates :nickname,        presence: :true, length: { in: 1..15 }
-  validates :introduction,    length: { maximum: 80 }
 
   default_scope ->            { order(created_at: "DESC") }
   enum status:                { active: 0, banned: 1, inactive: 2 }

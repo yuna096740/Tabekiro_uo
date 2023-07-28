898269c72884da8e8e19b13755a083ad708157d8
yuna096740 <129647184+yuna096740@users.noreply.github.com>
2023-07-28 07:58:28 +0000

    [fix] host in development.rb

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -34,7 +34,7 @@ Rails.application.configure do
   config.active_storage.service = :local
 
   # Don't care if the mailer can't send.
-  config.action_mailer.default_url_options = { host: 'tabekirouo.com' }
+  config.action_mailer.default_url_options = { :host => 'tabekirouo.com' }
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
